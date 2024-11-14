daf45804c5604e95e70644f0b4c7a8de92b34402
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2024-11-14 11:38:35 -0300

    first commit

---
+++ dsus.js
@@ -0,0 +1,9 @@
+class Desenvolvedor {
+    constructor(IDdesenvolvedor, idade, nome, codificacao){
+        this.IDdesenvolvedor = IDdesenvolvedor
+        this.idade = idade
+        this.nome = nome
+        this.codificacao = codificacao
+    }
+}
+
