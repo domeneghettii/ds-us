abfb70f6eac43e44978f4789af90aa2916fb33d8
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2024-11-14 11:46:27 -0300

    feate: criação de todas as classes

--- dsus.js
+++ dsus.js
@@ -7,3 +7,37 @@ class Desenvolvedor {
     }
 }
 
+class Jogador {
+    constructor(nome, IDusuario, genero, idade){
+        this.nome = nome;
+        this.IDusuario = IDusuario;
+        this.genero = genero;
+        this.idade = idade;
+    }
+}
+
+class Missoes {
+    constructor(pontuacao, IDmissao, tarefas, descricao){
+        this.pontuacao = pontuacao;
+        this.IDmissao = IDmissao;
+        this.tarefas = tarefas;
+        this.descricao = descricao;
+    }
+}
+
+class Sala {
+    constructor(NomeSala, IDsala, pontuacao){
+        this.NomeSala = NomeSala;
+        this.IDsala = IDsala;
+        this.pontuacao = pontuacao;
+    }
+}
+
+class Impostor {
+    constructor(sabotagens, capacidadeMax, pontuacao, IDimpostor){
+        this.sabotagens = sabotagens;
+        this.capacidadeMax = capacidadeMax;
+        this.pontuacao = pontuacao;
+        this.IDimpostor = IDimpostor;
+    }
+}
